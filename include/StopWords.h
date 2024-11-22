e1afe6387e4c93a274f9f9467adcac0c02abbc11
SMUAmeen <159220726+SMUAmeen@users.noreply.github.com>
2024-11-22 16:56:43 -0600

    Update StopWords.h

--- include/StopWords.h
+++ include/StopWords.h
@@ -4,15 +4,19 @@
 #include <string>
 #include <unordered_set>
 
+// StopWords class for managing a set of stop words and checking if a word is a stop word
 class StopWords {
 public:
-    StopWords();
+    StopWords(); // Constructor to initialize the stop words set
     
+    // Checks if a given word is a stop word
     bool isStopWord(const std::string& word) const;
 
 private:
-    std::unordered_set<std::string> stopWordsSet;
+    std::unordered_set<std::string> stopWordsSet; // Stores the set of stop words
+    
+    // Loads a default list of stop words into the stopWordsSet
     void loadDefaultStopWords();
 };
 
-#endif 
+#endif
