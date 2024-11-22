7781ae57ce64cb3aaf590ad69d5282940abef734
adhuynh <167336952+adhuynh@users.noreply.github.com>
2024-11-22 17:41:15 -0600

    Update StopWords.h

--- include/StopWords.h
+++ include/StopWords.h
@@ -4,15 +4,22 @@
 #include <string>
 #include <unordered_set>
 
+// The StopWords class is responsible for managing a collection of stop words
+// (commonly used words like "the", "is", "and") that are excluded during text processing.
 class StopWords {
 public:
+    // Constructor initializes the stopWordsSet with a default list of common stop words
     StopWords();
-    
+
+    // Checks if a given word is a stop word
+    // The comparison is case-insensitive
     bool isStopWord(const std::string& word) const;
 
 private:
-    std::unordered_set<std::string> stopWordsSet;
+    std::unordered_set<std::string> stopWordsSet; // Stores the stop words for quick lookup
+
+    // Loads a default set of stop words into the stopWordsSet
     void loadDefaultStopWords();
 };
 
-#endif 
+#endif
